1410055eae2ddbae22ad607fbd6c003d249edbde
Ginny Tran <ginny@powerkernel.com>
2023-03-11 18:37:41 +0000

    feat: adds article category

---
+++ src/types/strapi-category.ts
@@ -0,0 +1,13 @@
+type StrapiCategory = {
+  id: number;
+  attributes: {
+    title: string;
+    slug: string;
+    description: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+  };
+};
+
+export default StrapiCategory;
