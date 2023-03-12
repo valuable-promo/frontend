5b112d94c580c489fae749cf4057cdd380c92a00
Ginny Tran <ginny@powerkernel.com>
2023-03-12 13:30:12 +0000

    feat: allows specify image formats

--- src/lib/media.ts
+++ src/lib/media.ts
@@ -1,17 +1,28 @@
 // local
 import { getStrapiURL } from './api';
 // types
-import StrapiMedia from '@/types/strapi-media';
+import { StrapiMedia, StrapiMediaFormat } from '@/types/strapi-media';
 
 /**
  * Get full Strapi Media URL from a Strapi Media object
  * @param {StrapiMedia} media Strapi Media Object
- * @returns {string} Full Strapi Media URL
  */
-const getStrapiMedia = (media: StrapiMedia) => {
+const getStrapiMedia = (media: StrapiMedia, format?: StrapiMediaFormat) => {
   const { url } = media.attributes;
-  const imageUrl = url.startsWith('/') ? getStrapiURL(url) : url;
-  return imageUrl;
+  if (format && media.attributes.formats[format]) {
+    return {
+      url: media.attributes.formats[format]!.url.startsWith('/')
+        ? getStrapiURL(url)
+        : media.attributes.formats[format]!.url,
+      width: media.attributes.formats[format]!.width,
+      height: media.attributes.formats[format]!.height,
+    };
+  }
+  return {
+    url: url.startsWith('/') ? getStrapiURL(url) : url,
+    width: media.attributes.width,
+    height: media.attributes.height,
+  };
 };
 
 export { getStrapiMedia };
