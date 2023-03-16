71c72ad930b680cfa1c3a7df75773025fc057801
Ginny Tran <ginny@powerkernel.com>
2023-03-16 11:23:38 +0000

    feat: adds storage and strapi envs

--- next.config.js
+++ next.config.js
@@ -3,12 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     loader: 'default',
-    domains: [
-      'localhost',
-      'host.docker.internal',
-      'valuable-promo-headless-dev.s3.nl-ams.scw.cloud',
-      'valuable-promo-headless-prod.s3.nl-ams.scw.cloud',
-    ],
+    domains: ['localhost', 'host.docker.internal', process.env.NEXT_PUBLIC_STORAGE_HOST],
   },
 };
 
