3c643df60c54de4860fc72971abb576fd1b5cbcd
Harry Tang <harry@powerkernel.com>
2023-03-23 17:20:40 +0000

    feat: loads site name from strapi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,24 @@
 // local
 import Footer from '@/components/footer';
+import { fetchAPI } from '@/lib/api';
 import '@/styles/globals.css';
+import { Metadata } from 'next';
+import type Global from '@/types/global';
 
-export const metadata = {
-  title: {
-    default: process.env.SITE_NAME,
-    template: `%s | ${process.env.SITE_NAME}`,
-  },
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const global = await getGlobal();
+  return {
+    title: {
+      default: global.attributes.siteName,
+      template: `%s | ${global.attributes.siteName}`,
+    },
+  };
+}
+
+async function getGlobal() {
+  const globalRes = await fetchAPI<Global>('/global');
+  return globalRes.data;
+}
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
