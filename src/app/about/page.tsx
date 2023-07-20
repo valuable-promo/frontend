8d1eeedc9b54de2b884f4029d3b4bf51db2a4c96
Harry Tang <harry@powerkernel.com>
2023-07-20 11:38:34 +0000

    chore: use h1 in about page instead of h2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -29,7 +29,7 @@ const Page = async () => {
     <div className="bg-white py-32">
       <div className="mx-auto max-w-2xl ">
         <article className="prose lg:prose-xl">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{about.attributes.title}</h2>
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{about.attributes.title}</h1>
           <ReactMarkdown className="mt-6 text-lg leading-8 text-gray-600 markdown">
             {about.attributes.content}
           </ReactMarkdown>
